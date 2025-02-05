e4e5b7631f20f98c8896d0da08de9fc9d261c11b
danielRaoli <danielraoli074@gmail.com>
2025-02-05 11:28:47 -0300

    feat: update dashboar method

--- InfoFretamento/Infrastructure/Repositories/DashBoardRepository.cs
+++ InfoFretamento/Infrastructure/Repositories/DashBoardRepository.cs
@@ -76,17 +76,9 @@ namespace InfoFretamento.Infrastructure.Repositories
                })
                .ToListAsync();
 
-            var receitasViagem = await _context.Passagens.AsNoTracking().Include(p => p.Viagem)
-               .Where(p => p.Viagem.Saida.Data.Year == ano) // Filtra receitas pelo ano
-               .GroupBy(p => p.Viagem.Saida.Data.Month)    // Agrupa por mês
-               .Select(g => new
-               {
-                   Month = g.Key,
-                   Total = g.Sum(p => p.ValorTotal) // Soma os valores das receitas no mês
-               })
-               .ToListAsync();
 
-            var despesasMensais = await _context.PagamentosDespesa
+
+            var despesasMensais = await _context.PagamentosDespesa.AsNoTracking()
                         .Where(d => d.DataPagamento.Year == ano) // Filtra despesas pelo ano
                         .GroupBy(d => d.DataPagamento.Month)    // Agrupa por mês
                         .Select(g => new
@@ -96,7 +88,7 @@ namespace InfoFretamento.Infrastructure.Repositories
                         })
                 .ToListAsync();
 
-            var boletosDespesas = await _context.Boletos
+            var boletosDespesas = await _context.Boletos.AsNoTracking()
             .Where(d => d.Pago == true && d.DataPagamento.Value.Year == ano) // Filtra despesas pelo ano
             .GroupBy(d => d.DataPagamento.Value.Month)    // Agrupa por mês
             .Select(g => new
@@ -106,23 +98,46 @@ namespace InfoFretamento.Infrastructure.Repositories
             })
             .ToListAsync();
 
+            var gastosAbastecimentos = await _context.Abastecimentos.AsNoTracking()
+            .Where(d =>  d.DataPagamento.Year == ano) // Filtra despesas pelo ano
+            .GroupBy(d => d.DataPagamento.Month)    // Agrupa por mês
+            .Select(g => new
+            {
+                Month = g.Key,
+                Total = g.Sum(d => d.ValorTotal) // Soma os valores das despesas no mês
+            })
+            .ToListAsync();
+
+            var gastosAdiantamentos = await _context.Adiantamentos.AsNoTracking().Include(a => a.Viagem)
+           .Where(d => d.Viagem.DataHorarioSaida.Data.Year == ano) // Filtra despesas pelo ano
+           .GroupBy(d => d.Viagem.DataHorarioSaida.Data.Month)    // Agrupa por mês
+           .Select(g => new
+           {
+               Month = g.Key,
+               Total = g.Sum(d => d.ValorDeAcerto) // Soma os valores das despesas no mês
+           })
+           .ToListAsync();
+
             var salarios = await _context.Salario.AsNoTracking().Select(s => s.ValorTotal).SumAsync();
             var desepsasaFixas = await _context.DespesaMensal.AsNoTracking().Select(d => d.ValorTotal).SumAsync();
             // Combina receitas e despesas para cada mês
             var resumoMensal = Enumerable.Range(1, 12).Select(mes =>
                 {
                     var receitas = receitasMensais.FirstOrDefault(r => r.Month == mes)?.Total ?? 0;
-                    var receitasPassagens = receitasViagem.FirstOrDefault(r => r.Month == mes)?.Total ?? 0;
 
                     var despesas = despesasMensais.FirstOrDefault(d => d.Month == mes)?.Total ?? 0;
                     var boletos = boletosDespesas.FirstOrDefault(b => b.Month == mes)?.Total ?? 0;
+                    var abastecimentos = gastosAbastecimentos.FirstOrDefault(a => a.Month == mes)?.Total ?? 0;
+                    var adiantamentos =  gastosAdiantamentos.FirstOrDefault(a => a.Month ==mes)?.Total ?? 0;
+
+                    var totalDespesa = despesas + boletos + salarios + desepsasaFixas + abastecimentos + adiantamentos,
 
                     return new ReceitasMensais
                     {
                         Month = new DateTime(ano, mes, 1).ToString("MMM", new System.Globalization.CultureInfo("pt-BR")), // Nome reduzido do mês
-                        Receitas = receitas + receitasPassagens,
-                        Despesas = despesas + boletos +salarios +desepsasaFixas,     
-                        ValorLiquido = receitas - despesas
+                        Receitas = receitas ,
+                        Despesas =  totalDespesa,
+                        ValorLiquido = receitas - totalDespesa,
                     };
                 }).ToList();
 
